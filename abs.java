294128c1036bcff6d1fce0672a32b1eb84193c40
1BM18CS114 <54112164+1BM18CS114@users.noreply.github.com>
2019-09-26 15:18:11 +0530

    Update abs.java

--- abs.java
+++ abs.java
@@ -65,42 +65,40 @@ class abs{
 	public static void main(String arg[]){
 		int a;
 		int b;
-		System.out.println("Enter 1: Rectangle, 2: Square, 3: Circle, 4: Triangle");
-		Scanner scan = new Scanner(System.in);
-		int x = scan.nextInt();
-		switch(x){
-			case 1: 
-				System.out.println("Enter Values for Rectangle");
-				a = scan.nextInt();
-				b = scan.nextInt();
-				Rectangle r = new Rectangle(a, b);
-				r.calc();
-				break;
-			case 2:
-				System.out.println("Enter Values for Square");
-				a = scan.nextInt();
-				Square s = new Square(a);
-				s.calc();
-				break;
-			case 3:
-				System.out.println("Enter Values for Circle");
-				a = scan.nextInt();
-				Circle c = new Circle(a);
-				c.calc();
-				break;
-			case 4:
-				System.out.println("Enter Values for Triangle");
-				a = scan.nextInt();
-				b = scan.nextInt();
-				Triangle t = new Triangle(a, b);
-				t.calc();
-				break;
+		int x = 0;
+		while(x != 5){
+			System.out.println("Enter 1: Rectangle, 2: Square, 3: Circle, 4: Triangle, 5: Exit");
+			Scanner scan = new Scanner(System.in);
+			x = scan.nextInt();
+			switch(x){
+				case 1: 
+					System.out.println("Enter Values for Rectangle");
+					a = scan.nextInt();
+					b = scan.nextInt();
+					Rectangle r = new Rectangle(a, b);
+					r.calc();
+					break;
+				case 2:
+					System.out.println("Enter Values for Square");
+					a = scan.nextInt();
+					Square s = new Square(a);
+					s.calc();
+					break;
+				case 3:
+					System.out.println("Enter Values for Circle");
+					a = scan.nextInt();
+					Circle c = new Circle(a);
+					c.calc();
+					break;
+				case 4:
+					System.out.println("Enter Values for Triangle");
+					a = scan.nextInt();
+					b = scan.nextInt();
+					Triangle t = new Triangle(a, b);
+					t.calc();
+					break;
+				}
 			}
 		}
 	}
-	
-	
-	
-
-	
 	
