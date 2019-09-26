aee8892747dafa397a22bd2185cbce121a922c97
1BM18CS114 <54112164+1BM18CS114@users.noreply.github.com>
2019-09-26 15:27:21 +0530

    Update abs.java

--- abs.java
+++ abs.java
@@ -67,32 +67,34 @@ class abs{
 		int b;
 		int x = 0;
 		while(x != 5){
-			System.out.println("Enter 1: Rectangle, 2: Square, 3: Circle, 4: Triangle, 5: Exit");
+			System.out.println("\nEnter 1: Rectangle, 2: Square, 3: Circle, 4: Triangle, 5: Exit\n");
 			Scanner scan = new Scanner(System.in);
 			x = scan.nextInt();
 			switch(x){
 				case 1: 
-					System.out.println("Enter Values for Rectangle");
+					System.out.println("Enter Value a for Rectangle");
 					a = scan.nextInt();
+					System.out.println("Enter Value b for Rectangle");
 					b = scan.nextInt();
 					Rectangle r = new Rectangle(a, b);
 					r.calc();
 					break;
 				case 2:
-					System.out.println("Enter Values for Square");
+					System.out.println("Enter Value a for Square");
 					a = scan.nextInt();
 					Square s = new Square(a);
 					s.calc();
 					break;
 				case 3:
-					System.out.println("Enter Values for Circle");
+					System.out.println("Enter Value a for Circle");
 					a = scan.nextInt();
 					Circle c = new Circle(a);
 					c.calc();
 					break;
 				case 4:
-					System.out.println("Enter Values for Triangle");
+					System.out.println("Enter Value a for Triangle");
 					a = scan.nextInt();
+					System.out.println("Enter Value b for Triangle");
 					b = scan.nextInt();
 					Triangle t = new Triangle(a, b);
 					t.calc();
